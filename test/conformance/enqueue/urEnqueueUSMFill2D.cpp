2a044ecc60514531048e9914918d9d12b996413a
Brandon Yates <brandon.yates@intel.com>
2023-05-22 08:23:16 -0400

    Update test/conformance/enqueue/urEnqueueUSMFill2D.cpp
    Co-authored-by: Kenneth Benzie (Benie) <k.benzie83@gmail.com>

--- test/conformance/enqueue/urEnqueueUSMFill2D.cpp
+++ test/conformance/enqueue/urEnqueueUSMFill2D.cpp
@@ -154,7 +154,7 @@ struct urEnqueueUSMFill2DNegativeTest : uur::urQueueTest {
     void SetUp() override {
         UUR_RETURN_ON_FATAL_FAILURE(uur::urQueueTest::SetUp());
 
-        ur_device_usm_access_capability_flags_t device_usm = false;
+        ur_device_usm_access_capability_flags_t device_usm = 0;
         ASSERT_SUCCESS(uur::GetDeviceUSMDeviceSupport(device, device_usm));
         if (!device_usm) {
             GTEST_SKIP() << "Device USM is not supported";
