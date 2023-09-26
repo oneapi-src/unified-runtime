intel/llvm

[SYCL][NFC] Make UR_CHECK_ERROR a void return macro (#11100)
`UR_CHECK_ERROR` was designed to return `ur_result_t`, however in
practice it was guaranteed to only ever return `UR_RESULT_SUCCESS`, as
other paths would either terminate, abort or throw.

This in turns leads to poor quality/error prone code, as the codebase
was littered with:
* statements not checking the return value - depending on the compiler
generating a warning,
* extra check on the return which was only ever going to be true.

Some care was required, as the codebase has a habit of accumulating err
codes across branches, so depending on the use case the initial value of
`ur_result_t Result`s had to be set accordingly (now that
`UR_CHECK_ERROR` does not return).

common.cpp
@@ -28,10 +28,10 @@ ur_result_t mapErrorUR(hipError_t Result) {
   }
 }
 
-ur_result_t checkErrorUR(hipError_t Result, const char *Function, int Line,
-                         const char *File) {
+void checkErrorUR(hipError_t Result, const char *Function, int Line,
+                  const char *File) {
   if (Result == hipSuccess) {
-    return UR_RESULT_SUCCESS;
+    return;
   }
 
   if (std::getenv("SYCL_PI_SUPPRESS_ERROR_MESSAGE") == nullptr ||
@@ -56,10 +56,10 @@ ur_result_t checkErrorUR(hipError_t Result, const char *Function, int Line,
   throw mapErrorUR(Result);
 }
 
-ur_result_t checkErrorUR(ur_result_t Result, const char *Function, int Line,
-                         const char *File) {
+void checkErrorUR(ur_result_t Result, const char *Function, int Line,
+                  const char *File) {
   if (Result == UR_RESULT_SUCCESS) {
-    return UR_RESULT_SUCCESS;
+    return;
   }
 
   if (std::getenv("SYCL_PI_SUPPRESS_ERROR_MESSAGE") == nullptr ||

event.cpp
@@ -144,7 +144,8 @@ ur_result_t ur_event_handle_t_::record() {
       detail::ur::die(
           "Unrecoverable program state reached in event identifier overflow");
     }
-    Result = UR_CHECK_ERROR(hipEventRecord(EvEnd, Stream));
+    UR_CHECK_ERROR(hipEventRecord(EvEnd, Stream));
+    Result = UR_RESULT_SUCCESS;
   } catch (ur_result_t Error) {
     Result = Error;
   }
@@ -157,9 +158,9 @@ ur_result_t ur_event_handle_t_::record() {
 }
 
 ur_result_t ur_event_handle_t_::wait() {
-  ur_result_t Result;
+  ur_result_t Result = UR_RESULT_SUCCESS;
   try {
-    Result = UR_CHECK_ERROR(hipEventSynchronize(EvEnd));
+    UR_CHECK_ERROR(hipEventSynchronize(EvEnd));
     HasBeenWaitedOn = true;
   } catch (ur_result_t Error) {
     Result = Error;

oneapi-src/unified-runtime

common.cpp
@@ -33,10 +33,10 @@ ur_result_t mapErrorUR(CUresult Result) {
   }
 }
 
-ur_result_t checkErrorUR(CUresult Result, const char *Function, int Line,
-                         const char *File) {
+void checkErrorUR(CUresult Result, const char *Function, int Line,
+                  const char *File) {
   if (Result == CUDA_SUCCESS || Result == CUDA_ERROR_DEINITIALIZED) {
-    return UR_RESULT_SUCCESS;
+    return;
   }
 
   if (std::getenv("SYCL_PI_SUPPRESS_ERROR_MESSAGE") == nullptr &&
@@ -64,10 +64,10 @@ ur_result_t checkErrorUR(CUresult Result, const char *Function, int Line,
   throw mapErrorUR(Result);
 }
 
-ur_result_t checkErrorUR(ur_result_t Result, const char *Function, int Line,
-                         const char *File) {
+void checkErrorUR(ur_result_t Result, const char *Function, int Line,
+                  const char *File) {
   if (Result == UR_RESULT_SUCCESS) {
-    return UR_RESULT_SUCCESS;
+    return;
   }
 
   if (std::getenv("SYCL_PI_SUPPRESS_ERROR_MESSAGE") == nullptr &&

event.cpp
@@ -63,8 +63,8 @@ ur_result_t ur_event_handle_t_::start() {
   try {
     if (Queue->URFlags & UR_QUEUE_FLAG_PROFILING_ENABLE) {
       // NOTE: This relies on the default stream to be unused.
-      Result = UR_CHECK_ERROR(cuEventRecord(EvQueued, 0));
-      Result = UR_CHECK_ERROR(cuEventRecord(EvStart, Stream));
+      UR_CHECK_ERROR(cuEventRecord(EvQueued, 0));
+      UR_CHECK_ERROR(cuEventRecord(EvStart, Stream));
     }
   } catch (ur_result_t Err) {
     Result = Err;
@@ -112,7 +112,7 @@ ur_result_t ur_event_handle_t_::record() {
     return UR_RESULT_ERROR_INVALID_EVENT;
   }
 
-  ur_result_t Result = UR_RESULT_ERROR_INVALID_OPERATION;
+  ur_result_t Result = UR_RESULT_SUCCESS;
 
   UR_ASSERT(Queue, UR_RESULT_ERROR_INVALID_QUEUE);
 
@@ -122,7 +122,7 @@ ur_result_t ur_event_handle_t_::record() {
       detail::ur::die(
           "Unrecoverable program state reached in event identifier overflow");
     }
-    Result = UR_CHECK_ERROR(cuEventRecord(EvEnd, Stream));
+    UR_CHECK_ERROR(cuEventRecord(EvEnd, Stream));
   } catch (ur_result_t error) {
     Result = error;
   }
@@ -135,9 +135,9 @@ ur_result_t ur_event_handle_t_::record() {
 }
 
 ur_result_t ur_event_handle_t_::wait() {
-  ur_result_t Result;
+  ur_result_t Result = UR_RESULT_SUCCESS;
   try {
-    Result = UR_CHECK_ERROR(cuEventSynchronize(EvEnd));
+    UR_CHECK_ERROR(cuEventSynchronize(EvEnd));
     HasBeenWaitedOn = true;
   } catch (ur_result_t error) {
     Result = error;