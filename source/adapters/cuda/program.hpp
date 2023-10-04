88ef6ccf48f5d0bb2b267cce190b59f6c90db133
Fábio Mestre <fabio.mestre@codeplay.com>
2023-10-04 13:28:31 +0100

    [CUDA] Add support for binary type query

--- source/adapters/cuda/program.hpp
+++ source/adapters/cuda/program.hpp
@@ -24,6 +24,7 @@ struct ur_program_handle_t_ {
   size_t BinarySizeInBytes;
   std::atomic_uint32_t RefCount;
   ur_context_handle_t Context;
+  ur_program_binary_type_t BinaryType = UR_PROGRAM_BINARY_TYPE_NONE;
 
   /* The ur_program_binary_type_t property is defined individually for every
    * device in a program. However, since the CUDA adapter only has 1 device per
