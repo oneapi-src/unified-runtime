03cd95b4a743388517cd2471c47ec3edfd13879e
omarahmed1111 <omarpiratee2010@gmail.com>
2023-11-30 17:48:46 +0000

    Add device handle to opencl

--- source/adapters/opencl/context.hpp
+++ source/adapters/opencl/context.hpp
@@ -16,3 +16,22 @@ ur_result_t
 getDevicesFromContext(ur_context_handle_t hContext,
                       std::unique_ptr<std::vector<cl_device_id>> &DevicesInCtx);
 }
+
+// struct ur_context_handle_t_ {
+//     using native_type = cl_context;
+//     native_type Context;
+//     std::atomic_uint32_t RefCount;
+//     ur_platform_handle_t Platform;
+
+//     ur_context_handle_t_(native_type Ctx):Context(Ctx) {}
+
+//     ~ur_context_handle_t_() {}
+
+//     native_type get() { return Context; }
+
+//     uint32_t incrementReferenceCount() noexcept { return ++RefCount; }
+
+//     uint32_t decrementReferenceCount() noexcept { return --RefCount; }
+
+//     uint32_t getReferenceCount() const noexcept { return RefCount; }
+// };
