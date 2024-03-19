fa530f2de08fdec6dae17d92aa83ca9f497aad87
PietroGhg <pietro.ghiglio@codeplay.com>
2024-03-19 15:36:03 +0000

    formatting

--- source/adapters/native_cpu/enqueue.cpp
+++ source/adapters/native_cpu/enqueue.cpp
@@ -60,10 +60,10 @@ UR_APIEXPORT ur_result_t UR_APICALL urEnqueueKernelLaunch(
   }
 
   // Check reqd_work_group_size
-  if(hKernel->HasReqdWGSize && pLocalWorkSize != nullptr) {
-    const auto& Reqd = hKernel->ReqdWGSize;
-    for(uint32_t Dim = 0; Dim < workDim; Dim++) {
-      if(pLocalWorkSize[Dim] != Reqd[Dim]) {
+  if (hKernel->HasReqdWGSize && pLocalWorkSize != nullptr) {
+    const auto &Reqd = hKernel->ReqdWGSize;
+    for (uint32_t Dim = 0; Dim < workDim; Dim++) {
+      if (pLocalWorkSize[Dim] != Reqd[Dim]) {
         return UR_RESULT_ERROR_INVALID_WORK_GROUP_SIZE;
       }
     }
