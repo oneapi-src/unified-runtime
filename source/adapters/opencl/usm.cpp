cd29bd7ed575b176a2b5be1ff752d41ff16c9a77
Lorenc Bushi <lorenc.bushi@intel.com>
2024-05-27 09:24:30 -0700

    Remove assertions to instead return error code

--- source/adapters/opencl/usm.cpp
+++ source/adapters/opencl/usm.cpp
@@ -116,9 +116,11 @@ urUSMHostAlloc(ur_context_handle_t hContext, const ur_usm_desc_t *pUSMDesc,
 
   *ppMem = Ptr;
 
-  assert((Alignment == 0 ||
-          reinterpret_cast<std::uintptr_t>(*ppMem) % Alignment == 0) &&
-         "Allocation not aligned correctly!");
+  if (!(Alignment == 0 ||
+        reinterpret_cast<std::uintptr_t>(*ppMem) % Alignment == 0)) {
+    urUSMFree(hContext, Ptr);
+    return UR_RESULT_ERROR_UNSUPPORTED_ALIGNMENT;
+  }
 
   return UR_RESULT_SUCCESS;
 }
@@ -159,9 +161,11 @@ urUSMDeviceAlloc(ur_context_handle_t hContext, ur_device_handle_t hDevice,
 
   *ppMem = Ptr;
 
-  assert((Alignment == 0 ||
-          reinterpret_cast<std::uintptr_t>(*ppMem) % Alignment == 0) &&
-         "Allocation not aligned correctly!");
+  if (!(Alignment == 0 ||
+        reinterpret_cast<std::uintptr_t>(*ppMem) % Alignment == 0)) {
+    urUSMFree(hContext, Ptr);
+    return UR_RESULT_ERROR_UNSUPPORTED_ALIGNMENT;
+  }
 
   return UR_RESULT_SUCCESS;
 }
@@ -202,9 +206,11 @@ urUSMSharedAlloc(ur_context_handle_t hContext, ur_device_handle_t hDevice,
 
   *ppMem = Ptr;
 
-  assert((Alignment == 0 ||
-          reinterpret_cast<std::uintptr_t>(*ppMem) % Alignment == 0) &&
-         "Allocation not aligned correctly!");
+  if (!(Alignment == 0 ||
+        reinterpret_cast<std::uintptr_t>(*ppMem) % Alignment == 0)) {
+    urUSMFree(hContext, Ptr);
+    return UR_RESULT_ERROR_UNSUPPORTED_ALIGNMENT;
+  }
   return UR_RESULT_SUCCESS;
 }
 
