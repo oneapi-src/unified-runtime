d2f85236cb4f24cc56d8fb56cf36da51e5ecad44
Igor Chorazewicz <igor.chorazewicz@intel.com>
2024-07-24 18:42:26 +0000

    [L0 v2] make destructor of the legacy context virtual
    so that v2::context will be appropriately destroyed

--- source/adapters/level_zero/context.hpp
+++ source/adapters/level_zero/context.hpp
@@ -43,6 +43,9 @@ struct ur_context_handle_t_ : _ur_object {
 
   ur_context_handle_t_(ze_context_handle_t ZeContext) : ZeContext{ZeContext} {}
 
+  // Make sure this is virtual so that v2::context is appropriately destroyed
+  virtual ~ur_context_handle_t_() {}
+
   // A L0 context handle is primarily used during creation and management of
   // resources that may be used by multiple devices.
   // This field is only set at ur_context_handle_t creation time, and cannot
