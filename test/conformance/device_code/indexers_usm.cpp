7f3baabe52e1fbf90a5aa2b0891ee23f20ba57d7
Martin Morrison-Grant <martin.morrisongrant@codeplay.com>
2024-08-22 15:45:30 +0100

    Fix include warning and a SYCL nd_range deprecation warning in a CTS test - indexers_usm.cpp.

--- test/conformance/device_code/indexers_usm.cpp
+++ test/conformance/device_code/indexers_usm.cpp
@@ -9,7 +9,7 @@ int main() {
     const sycl::range<3> global_range(8, 8, 8);
     const sycl::range<3> local_range(2, 2, 2);
     const sycl::id<3> global_offset(4, 4, 4);
-    const sycl::nd_range<3> nd_range(global_range, local_range, global_offset);
+    const sycl::nd_range<3> nd_range(global_range, local_range);
 
     sycl::queue sycl_queue;
     const size_t elements_per_work_item = 6;
@@ -20,13 +20,16 @@ int main() {
 
     sycl_queue.submit([&](sycl::handler &cgh) {
         cgh.parallel_for<class indexers>(
-            nd_range, [ptr](sycl::nd_item<3> index) {
+            nd_range, [ptr, global_offset](sycl::nd_item<3> index) {
+                sycl::id<3> adjusted_global_id =
+                    index.get_global_id() - global_offset;
+
                 int *wi_ptr =
                     ptr + index.get_global_linear_id() * elements_per_work_item;
 
-                wi_ptr[0] = index.get_global_id(0);
-                wi_ptr[1] = index.get_global_id(1);
-                wi_ptr[2] = index.get_global_id(2);
+                wi_ptr[0] = adjusted_global_id[0];
+                wi_ptr[1] = adjusted_global_id[1];
+                wi_ptr[2] = adjusted_global_id[2];
 
                 wi_ptr[3] = index.get_local_id(0);
                 wi_ptr[4] = index.get_local_id(1);
