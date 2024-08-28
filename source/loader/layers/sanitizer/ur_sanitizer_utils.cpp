7ede9accd3e55da5ef7a231926d280e1ff4fd9ec
Wu Yingcong <yingcong.wu@intel.com>
2024-08-28 17:24:01 +0800

    Update source/loader/layers/sanitizer/ur_sanitizer_utils.cpp
    Co-authored-by: Yang Zhao <allanzyne@outlook.com>

--- source/loader/layers/sanitizer/ur_sanitizer_utils.cpp
+++ source/loader/layers/sanitizer/ur_sanitizer_utils.cpp
@@ -74,7 +74,7 @@ ur_device_handle_t GetDevice(ur_queue_handle_t Queue) {
 
 std::vector<ur_device_handle_t> GetDevices(ur_context_handle_t Context) {
     std::vector<ur_device_handle_t> Devices{};
-    uint32_t DeviceNum;
+    uint32_t DeviceNum = 0;
     [[maybe_unused]] ur_result_t Result;
     Result = getContext()->urDdiTable.Context.pfnGetInfo(
         Context, UR_CONTEXT_INFO_NUM_DEVICES, sizeof(uint32_t), &DeviceNum,
