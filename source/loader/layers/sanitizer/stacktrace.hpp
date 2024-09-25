6f67b1c61a731fa023b393f6cfd2f859897ab6c7
Maosu Zhao <maosu.zhao@intel.com>
2024-09-25 14:33:43 +0800

    [DeviceSanitizer] Only try to get backtrace symbols when needed

--- source/loader/layers/sanitizer/stacktrace.hpp
+++ source/loader/layers/sanitizer/stacktrace.hpp
@@ -18,14 +18,16 @@
 
 namespace ur_sanitizer_layer {
 
-constexpr size_t MAX_BACKTRACE_FRAMES = 64;
+constexpr size_t MAX_BACKTRACE_FRAMES = 48;
 
 struct StackTrace {
-    std::vector<BacktraceInfo> stack;
+    std::vector<BacktraceFrame> stack;
 
     void print() const;
 };
 
 StackTrace GetCurrentBacktrace();
+
+char **GetBacktraceSymbols(const std::vector<BacktraceFrame> &BacktraceFrames);
 
 } // namespace ur_sanitizer_layer
