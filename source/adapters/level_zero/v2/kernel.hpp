62e9941d2a8401cbb7eeafc5f2fb3e1d726368d6
Igor Chorazewicz <igor.chorazewicz@intel.com>
2024-10-15 00:33:18 +0200

    [L0 v2] implement native APIs for kernel, context and memory

--- source/adapters/level_zero/v2/kernel.hpp
+++ source/adapters/level_zero/v2/kernel.hpp
@@ -13,6 +13,7 @@
 #include "../program.hpp"
 
 #include "common.hpp"
+#include "memory.hpp"
 
 struct ur_single_device_kernel_t {
   ur_single_device_kernel_t(ur_device_handle_t hDevice,
@@ -26,6 +27,8 @@ struct ur_single_device_kernel_t {
 
 struct ur_kernel_handle_t_ : _ur_object {
 private:
+  struct pending_memory_allocation_t;
+
 public:
   ur_kernel_handle_t_(ur_program_handle_t hProgram, const char *kernelName);
 
@@ -37,11 +40,11 @@ public:
   // Get L0 kernel handle for a given device
   ze_kernel_handle_t getZeHandle(ur_device_handle_t hDevice);
 
+  // Get handle of the kernel for urKernelGetNativeHandle.
+  ze_kernel_handle_t getNativeZeHandle() const;
+
   // Get program handle of the kernel.
   ur_program_handle_t getProgramHandle() const;
-
-  // Get devices the kernel is built for.
-  std::vector<ur_device_handle_t> getDevices() const;
 
   // Get name of the kernel.
   const std::string &getName() const;
@@ -67,15 +70,38 @@ public:
   // Perform cleanup.
   ur_result_t release();
 
+  // Add a pending memory allocation for which device is not yet known.
+  void addPendingMemoryAllocation(pending_memory_allocation_t allocation);
+
+  // Set all required values for the kernel before submission (including pending
+  // memory allocations).
+  ur_result_t
+  prepareForSubmission(ur_context_handle_t hContext, ur_device_handle_t hDevice,
+                       const size_t *pGlobalWorkOffset, uint32_t workDim,
+                       uint32_t groupSizeX, uint32_t groupSizeY,
+                       uint32_t groupSizeZ,
+                       std::function<void(void *, void *, size_t)> migrate);
+
 private:
   // Keep the program of the kernel.
-  ur_program_handle_t hProgram;
+  const ur_program_handle_t hProgram;
 
-  // Vector of ur_single_device_kernel_t indexed by device->Id
+  // Vector of ur_single_device_kernel_t indexed by deviceIndex().
   std::vector<std::optional<ur_single_device_kernel_t>> deviceKernels;
 
   // Cache of the kernel name.
   mutable ZeCache<std::string> zeKernelName;
+
+  // Index of the device in the deviceKernels vector.
+  size_t deviceIndex(ur_device_handle_t hDevice) const;
+
+  struct pending_memory_allocation_t {
+    ur_mem_handle_t hMem;
+    ur_mem_handle_t_::access_mode_t mode;
+    uint32_t argIndex;
+  };
+
+  std::vector<pending_memory_allocation_t> pending_allocations;
 
   void completeInitialization();
 };
