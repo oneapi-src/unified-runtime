de98e9b31abfa24257c9ab68b78c6676cf1088f1
Uwe Dolinsky <uwe@codeplay.com>
2024-11-04 13:20:13 +0000

    [NATIVECPU] tbb to oneTBB

--- source/adapters/native_cpu/threadpool.hpp
+++ source/adapters/native_cpu/threadpool.hpp
@@ -238,7 +238,7 @@ using threadpool_t = simple_threadpool_t;
 
 } // namespace native_cpu
 
-#ifdef NATIVECPU_USE_TBB
+#ifdef NATIVECPU_WITH_ONETBB
 // Simple TBB backend
 #include "oneapi/tbb.h"
 namespace native_cpu {
