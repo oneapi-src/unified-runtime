637bd1340bd82190518b2978a70cb50fbd146cd1
Zhao, Yang2 <yang2.zhao@intel.com>
2024-11-20 23:44:14 -0800

    fix copyright

--- source/loader/layers/sanitizer/asan/asan_interceptor.cpp
+++ source/loader/layers/sanitizer/asan/asan_interceptor.cpp
@@ -1,7 +1,7 @@
 //===----------------------------------------------------------------------===//
 /*
  *
- * Copyright (C) 2023 Intel Corporation
+ * Copyright (C) 2024 Intel Corporation
  *
  * Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM Exceptions.
  * See LICENSE.TXT
