2aa1862c7ccc1816e1ce35ee179f3b44ab0f9014
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2024-12-05 16:21:29 +0100

    [UMF] Add missing copyright in disjoint_pool_config_parser.* files

--- source/common/umf_pools/disjoint_pool_config_parser.cpp
+++ source/common/umf_pools/disjoint_pool_config_parser.cpp
@@ -1,5 +1,7 @@
 //===--- disjoint_pool_config_parser.cpp -configuration for USM memory pool-==//
 //
+// Copyright (C) 2023-2024 Intel Corporation
+//
 // Part of the Unified-Runtime Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
